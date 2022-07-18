04447fb3fc6c1c2d908285c22275fbbbcbf77311
Martin Gonzalez <93826718+PaPitaPaloro@users.noreply.github.com>
2022-07-18 17:44:51 +0200

    se agrega el componente Nabvar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+
+
+
+const Navbar = () => {
+    
+    
+    return (
+        <nav style={{display: 'flex', backgroundColor: 'green'}}>
+            <div>
+                <h1>
+                    Caserito Caserito
+                </h1> 
+                <button>Catálogo</button>
+                <button>Sobre Nosotros</button>
+                <button>Contacto</button>
+            </div>
+            <div>
+
+            </div>
+        </nav>
+    )
+        
+    
+}
+
+
+export default Navbar
