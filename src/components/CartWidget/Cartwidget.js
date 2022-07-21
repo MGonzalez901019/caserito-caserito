12bba29f69ba4a0c1e8021ec5e854f216b1100de
Martin Gonzalez <93826718+PaPitaPaloro@users.noreply.github.com>
2022-07-21 16:31:28 +0200

    navbar y cartwidget

---
+++ src/components/CartWidget/Cartwidget.js
@@ -0,0 +1,12 @@
+import './CartWidget.css'
+
+const CartWidget = () => {
+  return (
+    <div >
+        <img className='CartWidget' src='images/shopping-cart.svg' alt='cartWidget'/>
+        10
+    </div>
+  )
+}
+
+export default CartWidget
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,23 +1,29 @@
+import './Navbar.css'
+import React from 'react'
+import CartWidget from '../CartWidget/Cartwidget'
 
 
+//TODO: agregar la libreria fontAwesome
+//TODO: poner el carrito a la derecha
 
 const Navbar = () => {
     
     
     return (
-        <nav style={{display: 'flex', backgroundColor: 'green'}}>
-            <div>
+
+
+        <nav className="Navbar" >
+            
                 <h1>
                     Caserito Caserito
                 </h1> 
                 <button>Catálogo</button>
                 <button>Sobre Nosotros</button>
                 <button>Contacto</button>
-            </div>
-            <div>
-
-            </div>
+                <CartWidget></CartWidget>
+                
         </nav>
+     
     )
         
     
